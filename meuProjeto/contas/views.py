c4877fceab79d98619cb3a0aaee7184ef3f3972a
Akheem CK <castrok@live.com>
2018-10-30 14:23:18 -0300

    :computer: seção 2

--- meuProjeto/contas/views.py
+++ meuProjeto/contas/views.py
@@ -1,3 +1,11 @@
+#from django.shortcuts import render
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+import datetime
+
+def home(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return render(request, 'contas/home.html')
---
+++ meuProjeto/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\inetpub\wwwroot\python\djangofundamentals\meuprojeto\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
