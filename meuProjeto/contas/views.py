38eae216d379232629baa3332a0862f8c414a8c9
Akheem CK <castrok@live.com>
2018-11-02 22:09:07 -0300

    :computer: Fim seção 2

--- meuProjeto/contas/views.py
+++ meuProjeto/contas/views.py
@@ -1,11 +1,16 @@
 #from django.shortcuts import render
 
 from django.http import HttpResponse
+
 from django.shortcuts import render
 
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'contas/home.html')
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data['now'] = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+
+    return render(request, 'contas/home.html', data)
